a5abb63ea36502f7137eb01da06e631544fd1fdd
David North <dtn@corefiling.com>
2024-07-11 10:42:13 +0100

    Diagnostic tool for TNWSP update

---
+++ tnwspupdate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+kubectl get deployment tnwsp-tnwsp -oyaml|sed "s#docker.corefiling.com/tnwsp/tnwsp:.*#docker.corefiling.com/tnwsp/tnwsp:2.86.0#g"|kubectl replace -f -
+
+echo "TNWSP version should now show as 2.86.0 below"
+
+kubectl get deployment tnwsp-tnwsp|grep tnwsp/tnwsp
